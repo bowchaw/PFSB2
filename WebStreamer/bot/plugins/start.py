9206345831fa26070608340248cc74a12bff8e1f
MoviesHuntHD <68388096+MoviesHuntHD@users.noreply.github.com>
2022-06-18 21:35:51 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -44,7 +44,7 @@ async def start(b, m):
                 if user.status == "kicked":
                     await b.send_message(
                         chat_id=m.chat.id,
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/JoinOT).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/StreamersOT).",
                         parse_mode="markdown",
                         disable_web_page_preview=True
                     )
@@ -66,7 +66,7 @@ async def start(b, m):
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+                    text="Something went Wrong. Contact my [Support Group](https://t.me/StreamersOT).",
                     parse_mode="markdown",
                     disable_web_page_preview=True)
                 return
@@ -87,7 +87,7 @@ async def start(b, m):
                 if user.status == "kicked":
                     await b.send_message(
                         chat_id=m.chat.id,
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/JoinOT).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/StreamersOT).",
                         parse_mode="markdown",
                         disable_web_page_preview=True
                     )
@@ -103,7 +103,7 @@ async def start(b, m):
                             ],
                             [
                                 InlineKeyboardButton("🔄 Refresh / Try Again",
-                                                     url=f"https://t.me/{(await b.get_me()).username}?start=AbirHasan2005_{usr_cmd}")
+                                                     url=f"https://t.me/{(await b.get_me()).username}?start=StreamersHub_{usr_cmd}")
                             ]
                         ]
                     ),
@@ -113,7 +113,7 @@ async def start(b, m):
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+                    text="Something went Wrong. Contact my [Support Group](https://t.me/StreamersOT).",
                     parse_mode="markdown",
                     disable_web_page_preview=True)
                 return
@@ -151,7 +151,7 @@ async def help_handler(bot, message):
             if user.status == "kicked":
                 await bot.send_message(
                     chat_id=message.chat.id,
-                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/JoinOT).",
+                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/StreamersOT).",
                     parse_mode="markdown",
                     disable_web_page_preview=True
                 )
@@ -173,7 +173,7 @@ async def help_handler(bot, message):
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/JoinOT).",
+                text="Something went Wrong. Contact my [Support Group](https://t.me/StreamersOT).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
@@ -183,8 +183,8 @@ async def help_handler(bot, message):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Support Group", url="https://t.me/JoinOT"), InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-                [InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005")]
+                [InlineKeyboardButton("Support Group", url="https://t.me/StreamersOT"), InlineKeyboardButton("Deals Channel", url="https://t.me/TheDealsMaster")],
+                [InlineKeyboardButton("Movies Channel", url="https://t.me/StreamersHub")]
             ]
         )
     )
